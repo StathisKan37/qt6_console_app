66f98e630d8d26815caa6f7740f463c40c8f0a89
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-06-24 13:35:13 -0400

    Add files via upload

---
+++ Qt6_02_Widget_app/mainwindow.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_Button_1_clicked()
+{
+    ui->label_1->setText("The button is clicked successfully!");
+}
